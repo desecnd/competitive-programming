27d85e253387a6b1ae4ee68498aef0bd6da17e09
pavveu7 <pawel@ewnet.pl>
2020-07-06 23:56:44 +0200

    added FOR macro and initialize matrix function to default.cpp

--- default.cpp
+++ default.cpp
@@ -14,16 +14,26 @@ using vll = vector<ll>;
 using pii = pair<int,int>;
 using graph = vector<vi>;
 
+#define FOR(name__, upper__) for (int (name__) = 0; (name__) < (upper__); ++(name__))
 #define all(x) begin(x), end(x)
 #define mp make_pair
 #define mt make_tuple
+
+template<class T>
+void initialize_matrix(vector<vector<T>>& matrix, int rows, int cols, T value) {
+	assert(matrix.empty());
+	FOR (row, rows) 
+		matrix.emplace_back(cols, value);
+}
 
 void go() {
 
 }
 
 int main() {
-	ios::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(false); 
+	cin.tie(0);
+
 	go();
 
 	return 0;
